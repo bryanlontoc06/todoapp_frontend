61bb2454a141fc5324523e7a29b0c84871d2ef9f
Bry <bryanlontoc06@gmail.com>
2023-02-02 06:08:43 +0800

    - add delete function

--- src/components/TodoComponent/TodoItem.js
+++ src/components/TodoComponent/TodoItem.js
@@ -1,36 +1,45 @@
 import React from 'react'
 import EditIcon from '@/components/svgs/EditIcon.svg'
 import DeleteIcon from '@/components/svgs/DeleteIcon.svg'
-import CheckIcon from '@/components/svgs/CheckIcon.svg'
 
-const TodoItem = ({todo}) => {
+const TodoItem = ({item, index, todos, setTodos}) => {
 
+  const handleEditItem = (id) => {
+    console.log('add')
+    console.log(id)
+  }
+
+  const handleDeleteItem = (id) => {
+    todos.splice(id, 1)
+    setTodos([...todos])
+    console.log(todos)
+  }
 
   return (
-    <div className="flex border-2 p-3 rounded-md">
+    <div className="flex border-2 p-3 rounded-md shadow-xl">
       <div className="flex gap-3 items-center">
           <input 
             type="checkbox" 
-            id="checkbox"
+            id={index}
             className="
               peer
               cursor-pointer 
               w-5 h-5 
               border-2 
               rounded-md 
-              focus: ring-green-300
+              focus: ring-0
               bg-sky-100
               border-sky-300
               text-sky-500
             "
           />
-        <label htmlFor="checkbox" className="cursor-pointer peer-checked:line-through peer-checked:text-gray-400">Test to do hahahah</label>
+        <label htmlFor={index} className="cursor-pointer peer-checked:line-through peer-checked:text-gray-400">{item}</label>
       </div>
       <div className="flex ml-auto gap-4">
-        <button>
+        <button onClick={() => handleEditItem(index)}>
           <EditIcon className="w-6 h-6 text-gray-500" />
         </button>
-        <button>
+        <button onClick={() => handleDeleteItem(index)}>
           <DeleteIcon className="w-6 h-6 text-red-500"/>
         </button>
       </div>
--- src/components/TodoComponent/index.js
+++ src/components/TodoComponent/index.js
@@ -2,11 +2,19 @@ import {useState} from 'react'
 import TodoItemComponent from './TodoItem'
 
 const Todo = () => {
-    const [todo, setTodo] = useState([])
+    const [todos, setTodos] = useState(['this is a todo 1', 
+    'this is a todo 2'
+    ])
+    const [todoItem, setTodoItem] = useState('')
 
     const handleSubmit = (e) => {
-        console.log('testtt')
         e.preventDefault()
+        setTodos([...todos, todoItem])
+        setTodoItem('')
+    }
+
+    const handleOnChange = (e) => {
+      setTodoItem(e.target.value)
     }
 
 
@@ -15,11 +23,25 @@ const Todo = () => {
         <form onSubmit={(e) => handleSubmit(e)} className="flex gap-5">
             <input type="text" className="shadow-xl block w-full px-3 py-2 rounded-md border-2 border-transparent focus:outline-none focus:ring-2 focus:ring-sky-600 focus:border-transparent"
             placeholder="Add Todo"
+            value={todoItem}
+            onChange={(e) => handleOnChange(e)}
             />
             <button className="shadow-lg py-2 px-5 rounded-3xl bg-sky-500 hover:bg-sky-400 text-white shadow-sky-500/50">Add</button>
         </form>
         <br />
-        <TodoItemComponent todo={todo} />
+        <div className="flex flex-col gap-2">
+          {todos.map((item, i) => {
+            return (
+              <TodoItemComponent 
+                key={i} 
+                index={i} 
+                item={item} 
+                todos={todos} 
+                setTodos={setTodos} 
+              />
+            )
+          })}
+        </div>
     </div>
   )
 }
