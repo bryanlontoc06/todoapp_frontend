7533739862a373c387ef80d1c5ca134da7919804
Bry <bryanlontoc06@gmail.com>
2023-02-08 00:23:55 +0800

    Update baseUrl.js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV !== 'production' ? "http://localhost:8000" : 'https://todoapp-backend-three.vercel.app';
+const baseUrl = process.env.NODE_ENV !== 'production' ? "http://localhost:8000" : 'todoappbackend-production-b84c.up.railway.app';
 
 module.exports = baseUrl;
